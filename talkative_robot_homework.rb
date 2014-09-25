c3bc26700fadf4b7402fdd0df6970a7a876084c5
canes6288 <joshkushner1@gmail.com>
2014-09-25 11:52:32 -0400

    Fixed that Grocery List request

--- talkative_robot_homework.rb
+++ talkative_robot_homework.rb
@@ -10,9 +10,8 @@ def get_user_info(user)
 
 	puts "What's your age?"
 	user[:age] = gets.chomp.to_i
-
-
 end
+
 
 def user_gender_response(user)
 	if user[:gender] == "m" 
@@ -21,6 +20,8 @@ def user_gender_response(user)
 		puts "Hey you're a girl!"
 	end
 end
+
+
 
 
 def jamaal_charles_age_comparison(user)
@@ -98,46 +99,46 @@ end
 
 user = {}
 
-get_user_info(user)
+# get_user_info(user)
 
-user_gender_response(user)
-jamaal_charles_age_comparison(user)
+# user_gender_response(user)
+# jamaal_charles_age_comparison(user)
 
-puts "Wow! You're old! Are you a great-great grandmother/father?" if user[:age] > 100
-puts "Wow! You're young!" unless user[:age] > 15
-
-
-age_75_comparison(user)
+# puts "Wow! You're old! Are you a great-great grandmother/father?" if user[:age] > 100
+# puts "Wow! You're young!" unless user[:age] > 15
 
 
-user[:gender_slang] = (user[:gender] == "m") ? ("guy") : ("girl")
-puts "Hey #{user[:name]}, you're a #{user[:gender_slang]} who is #{user[:age]} old!"
-
-user[:first_initial] = user[:name].chars.first
-puts "Do you mind if I call you #{user[:first_initial]}? (Yes, No)"
-user[:permission_response] = gets.chomp.downcase # Ask for it here
+# age_75_comparison(user)
 
 
-user[:permission_response] = incorrect_response_loop(user) # => will end up == "yes" or == "no"
-puts (user[:permission_response] == "yes") ? "OK, I won't call you that..." : "Cool!"
+# user[:gender_slang] = (user[:gender] == "m") ? ("guy") : ("girl")
+# puts "Hey #{user[:name]}, you're a #{user[:gender_slang]} who is #{user[:age]} old!"
+
+# user[:first_initial] = user[:name].chars.first
+# puts "Do you mind if I call you #{user[:first_initial]}? (Yes, No)"
+# user[:permission_response] = gets.chomp.downcase # Ask for it here
+
+
+# user[:permission_response] = incorrect_response_loop(user) # => will end up == "yes" or == "no"
+# puts (user[:permission_response] == "yes") ? "OK, I won't call you that..." : "Cool!"
 		
 
-user[:first_name_caps] = user[:name].upcase
-puts "Hey #{user[:first_name_caps]}, where are you going!?"	
-user[:destination] = gets.chomp
+# user[:first_name_caps] = user[:name].upcase
+# puts "Hey #{user[:first_name_caps]}, where are you going!?"	
+# user[:destination] = gets.chomp
 
-puts user[:destination] == "to wrestle a bear" ? "Jesus...you are awesome..." : "Eh, that sounds...OOOOkkkk..."
+# puts user[:destination] == "to wrestle a bear" ? "Jesus...you are awesome..." : "Eh, that sounds...OOOOkkkk..."
 
 
-puts 'Yo "Dude", what\'s up?'
-user[:whats_up] = gets.chomp
+# puts 'Yo "Dude", what\'s up?'
+# user[:whats_up] = gets.chomp
 
-puts "Hey, tell me your whole name (first, middle, last) and I'll tell you the 2nd letter of your middle name!"
-user[:full_name] = gets.chomp
+# puts "Hey, tell me your whole name (first, middle, last) and I'll tell you the 2nd letter of your middle name!"
+# user[:full_name] = gets.chomp
 
-user[:middle_name] = user[:full_name].split(" ")[1]
-user[:middle_name_second_letter] = user[:middle_name].chars[1].upcase
-puts "Hmmm is your second letter of your middle name a #{user[:middle_name_second_letter]}? I bet it is!"
+# user[:middle_name] = user[:full_name].split(" ")[1]
+# user[:middle_name_second_letter] = user[:middle_name].chars[1].upcase
+# puts "Hmmm is your second letter of your middle name a #{user[:middle_name_second_letter]}? I bet it is!"
 
 
 
@@ -148,10 +149,10 @@ delete_grabbed_item(user)
 
 
 
-puts user[:grocery_list]
+puts "Grocery list: #{user[:grocery_list].join(", ")}"
 
 puts "Oh yeah! Don't forget the Activia!"
 user[:grocery_list] << "activia"
-puts user[:grocery_list]
+puts "Grocery list: #{user[:grocery_list].join(", ")}"
 
 
